efc1e6d85c524132bf5accaafc0544856a690f75
AgustinPerezR <aperezr@fi.uba.ar>
2024-05-10 15:21:05 -0300

    creo esqueleto

---
+++ client_src/client_main.cpp
@@ -0,0 +1,24 @@
+
+int main() {
+
+    //conectarse al server
+    //inicializar cliente
+    //esperar confirmacion para iniciar (esperar a que se conecten los n clientes).
+
+
+    //inicializar interfaz grafica
+
+    //inicia bucle (hasta que el contador llegue a 0)
+        //leer de entrada estandar (qué teclas se presionan)
+        //recibir informacion de los estados y posiciones de todos los elementos
+        //actualizar estados y posiciones de todas las imagenes. (si no recibio nada, mantienen los que tenian)
+        //si mantuvieron el estado, avanzar al siguiente frame de cada animacion (segun corresponda)
+        //pintar
+
+        //hacer un sleep de ? milisegundos
+        //contador--
+
+    //finalmente
+    //mostrar tabla final y ganador.
+    return 0;
+}
