d5ee02ccefd6ddbe5b3030b26ebd2ce17a94502d
lorenzoahumada <lorenzosahumada@gmail.com>
2024-05-21 18:58:34 -0300

    Actualizo cmake y elimino archivos sin uso

---
+++ build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lorenzo/Escritorio/Taller/tp_final/client_src/client_main.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/client_src/client_main.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/client_src/client_player.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/client_src/client_player.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/client_src/client_protocol.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/client_src/client_protocol.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/client_src/client_receiver.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/client_src/client_receiver.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/client_src/client_sender.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/client_src/client_sender.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/client_src/interfaz_grafica.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/client_src/interfaz_grafica.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/client_src/sprite_object.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/client_src/sprite_object.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/client_src/spritesManager.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/client_src/spritesManager.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/client_src/spritesheet.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/client_src/spritesheet.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/client_src/spritesheetPlayer.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/client_src/spritesheetPlayer.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/common_src/liberror.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/common_src/liberror.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/common_src/resolver.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/common_src/resolver.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/common_src/resolvererror.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/common_src/resolvererror.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final/common_src/socket.cpp" "/home/lorenzo/Escritorio/Taller/tp_final/build/CMakeFiles/client.dir/home/lorenzo/Escritorio/Taller/tp_final/common_src/socket.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
