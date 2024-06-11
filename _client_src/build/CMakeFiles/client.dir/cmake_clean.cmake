b558c256e1467a386710335952dd22e93e06cb47
Esteban Frascarelli <esteban.frascarelli@kickads.mobi>
2024-06-11 17:47:05 -0300

    archivos mapa

---
+++ _client_src/build/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/_client_src/client_dummy_app.cpp.o"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/_client_src/client_dummy_app.cpp.o.d"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/_client_src/client_dummy_main.cpp.o"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/_client_src/client_dummy_main.cpp.o.d"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/_client_src/client_dummy_protocol.cpp.o"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/_client_src/client_dummy_protocol.cpp.o.d"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/common_src/liberror.cpp.o"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/common_src/liberror.cpp.o.d"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/common_src/resolver.cpp.o"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/common_src/resolver.cpp.o.d"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/common_src/resolvererror.cpp.o"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/common_src/resolvererror.cpp.o.d"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/common_src/socket.cpp.o"
+  "CMakeFiles/client.dir/home/esteban/Desktop/TallerDeProgramacion/jazz-jackrabbit-2/common_src/socket.cpp.o.d"
+  "client"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
