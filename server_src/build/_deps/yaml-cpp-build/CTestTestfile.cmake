8d754ba16221157eafd16216da79f1a6a45d1d5e
AgustinPerezR <aperezr@fi.uba.ar>
2024-06-15 21:54:45 -0300

    elimino server dummie

--- server_src/build/_deps/yaml-cpp-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/agustin/Escritorio/Taller 1/TP Final/jazz-jackrabbit-2/server_src/build/_deps/yaml-cpp-src
-# Build directory: /home/agustin/Escritorio/Taller 1/TP Final/jazz-jackrabbit-2/server_src/build/_deps/yaml-cpp-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
--- server_src/build/_deps/yaml-cpp-build/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/agustin/Escritorio/Taller 1/TP Final/jazz-jackrabbit-2/server_src/build/_deps/yaml-cpp-src/util
-# Build directory: /home/agustin/Escritorio/Taller 1/TP Final/jazz-jackrabbit-2/server_src/build/_deps/yaml-cpp-build/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
