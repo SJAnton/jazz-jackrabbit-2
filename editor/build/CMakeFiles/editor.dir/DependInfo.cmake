9c98952c01f953a4ae1334784eaf5471b5c29fc5
lorenzoahumada <lorenzosahumada@gmail.com>
2024-06-16 22:41:59 -0300

    Agrego readme y cambio ubicacion de build

---
+++ editor/build/CMakeFiles/editor.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/customgraphicsview.cpp" "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/build/CMakeFiles/editor.dir/customgraphicsview.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/build/editor_autogen/EWIEGA46WW/qrc_resources.cpp" "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/build/CMakeFiles/editor.dir/editor_autogen/EWIEGA46WW/qrc_resources.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/build/editor_autogen/mocs_compilation.cpp" "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/build/CMakeFiles/editor.dir/editor_autogen/mocs_compilation.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/leveleditor.cpp" "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/build/CMakeFiles/editor.dir/leveleditor.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/main.cpp" "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/build/CMakeFiles/editor.dir/main.cpp.o"
+  "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/map.cpp" "/home/lorenzo/Escritorio/Taller/tp_final_editor/editor/build/CMakeFiles/editor.dir/map.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "editor_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
